093fed7fa85d523fcf631db5b46017b2d680fcaa
FranzChoqueQuispe <132019579+FranzChoqueQuispe@users.noreply.github.com>
2023-07-18 11:24:47 -0400

    CLASE 1

--- game/components/game.py
+++ game/components/game.py
@@ -1,44 +1,47 @@
 import pygame
 
 from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
-
+from game.components.spaceship import Spaceship
 
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption(TITLE)
-        pygame.display.set_icon(ICON)
+        pygame.display.set_caption(TITLE)   #Titulo del juego
+        pygame.display.set_icon(ICON)       #Icono del Juego                  
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
-        self.game_speed = 10
+        self.game_speed = 10    #Velocidad del Juego
         self.x_pos_bg = 0
         self.y_pos_bg = 0
+        self.player = Spaceship()
 
-    def run(self):
-        # Game loop: events - update - draw
-        self.playing = True
+    def run(self):      
+        # Game loop: events - update - draw (Ciclo del Juego)
+        self.playing = True     #Iniciando el Juego
         while self.playing:
             self.events()
             self.update()
             self.draw()
-        pygame.display.quit()
+        pygame.display.quit()   #Salir del Juego
         pygame.quit()
 
     def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.playing = False
+        for event in pygame.event.get():    #Lista que devuelve los eventos
+            if event.type == pygame.QUIT:   #Verifica si existe un evento (QUIT: Si se pulsa el boton de cerrar)
+                self.playing = False        #Se cierra el juego
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((255, 255, 255))   #Fondo de pantalla
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
-        pygame.display.flip()
+        #pygame.display.flip()
 
     def draw_background(self):
         image = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
--- game/utils/constants.py
+++ game/utils/constants.py
@@ -2,14 +2,14 @@ import pygame
 import os
 
 # Global Constants
-TITLE = "Spaceships Game"
+TITLE = "Spaceships Game"  #Titulo del Juego 
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 FPS = 30
 IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 
 # Assets Constants
-ICON = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
+ICON = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png")) #Incono del Juego
 
 SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
 
